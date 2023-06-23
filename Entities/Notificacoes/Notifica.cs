f86ba0e9fd18972737d211806df326f4b7e0d4a5
gbcgu <gbcgu@GUILHERME>
2023-06-23 01:56:49 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Notificacoes/Notifica.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Notificacoes
+{
+    public class Notifica
+    {
+        public Notifica()
+        {
+            notificacoes = new List<Notifica>();
+        }
+
+        public string NomePropriedade { get; set; }
+
+        public string mensagem { get; set; }
+
+        public List<Notifica> notificacoes { get; set; }
+
+
+        public bool ValidaPropriedadeString(string valor, string nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                notificacoes.Add(new Notifica { mensagem = "Campo Obrigatório", NomePropriedade = nomePropriedade });
+                return false;
+            }
+            return true;
+        }
+
+        public bool ValidaPropriedadeInteiro(int valor, string nomePropriedade)
+        {
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                notificacoes.Add(new Notifica { mensagem = "Campo Obrigatório", NomePropriedade = nomePropriedade });
+                return false;
+            }
+            return true;
+        }
+    }
+}
