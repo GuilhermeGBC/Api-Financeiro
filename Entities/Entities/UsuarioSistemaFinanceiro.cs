537ee6070b89d4a7daa76d3a6c400b323a828473
Guilherme Bastos <guicorrea188827@gmail.com>
2023-06-23 13:29:43 -0300

    Adicionando entidades do sistema

--- Entities/Entities/UsuarioSistemaFinanceiro.cs
+++ Entities/Entities/UsuarioSistemaFinanceiro.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities.Entities
 {
-    public class ApplicationUser
+    internal class UsuarioSistemaFinanceiro
     {
-        [Column("USR_CPF")]
-        public string CPF { get; set; }
     }
 }
