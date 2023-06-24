0001f69bd13ba5fb008ec284e06e7d17225a99fb
Guilherme Bastos <guicorrea188827@gmail.com>
2023-06-24 20:20:23 -0300

    Add interfaces services

---
+++ Domain/Interfaces/InterfaceServices/IDespesaService.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.InterfaceServices
+{
+    public interface IDespesaService
+    {
+        Task AdicionarDespesa(Despesa despesa);
+        Task AtualizarDespesas(Despesa despesa);
+        Task CarregarGraficos(string emailUsuario);
+    }
+}
