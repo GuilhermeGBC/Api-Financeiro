632eb618e98a7484956df033f93cefe3d552f8d0
Guilherme Bastos <guicorrea188827@gmail.com>
2023-06-25 05:15:22 -0300

    add users controller

---
+++ WebApi/Controllers/UsersController.cs
@@ -0,0 +1,69 @@
+﻿using Entities.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("api/AdicionaUsuario")]
+        [Produces("application/json")]
+        public async Task<object> AdicionaUsuario([FromBody] Login login)
+        {
+            if(string.IsNullOrWhiteSpace(login.email) ||
+                string.IsNullOrWhiteSpace(login.senha) ||
+                string.IsNullOrWhiteSpace(login.cpf))
+            {
+                return Ok("Faltam alguns dados");
+            }
+            var user = new ApplicationUser
+            {
+                Email = login.email,
+                UserName = login.email,
+                CPF = login.cpf
+            };
+
+            var result = await _userManager.CreateAsync(user, login.senha);
+
+            if (result.Errors.Any())
+            {
+                return Ok(result.Errors);
+            }
+
+            //Para geração de confirmação caso seja necessário
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
+            //retorno do email
+            code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+
+            var response_return = await _userManager.ConfirmEmailAsync(user, code);
+
+            if (response_return.Succeeded)
+            {
+                return Ok("Usuário Adicionado");
+            }
+            else
+            {
+                return Ok("Erro ao confirmar cadastro de usuário");
+            }
+        }
+    }
+}
