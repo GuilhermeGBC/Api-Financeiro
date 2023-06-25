49fc95fdd1cf0146fb401509d51176d9df8faab8
Guilherme Bastos <guicorrea188827@gmail.com>
2023-06-25 04:20:08 -0300

    add DI

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,5 +1,12 @@
+using Domain.Interfaces.ICategoria;
+using Domain.Interfaces.IDespesa;
+using Domain.Interfaces.InterfaceServices;
+using Domain.Interfaces.ISistemaFinanceiro;
+using Domain.Interfaces.IUsuarioSistemaFinanceira;
+using Domain.Services;
 using Entities.Entities;
 using Infraestructure.Configuracao;
+using Infraestructure.Repo;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -20,6 +27,19 @@ builder.Services.AddDbContext<ContextBase>(options =>
 });
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ContextBase>();
+
+// INTERFACE E REPOSITORIOS
+builder.Services.AddSingleton<ICategoriaService, CategoriaService>();
+builder.Services.AddSingleton<IDespesaService, DespesaService>();
+builder.Services.AddSingleton<IUsuarioSistemaFinanceiroService, UsuarioSistemaFinanceiroService>();
+builder.Services.AddSingleton<ISistemaFinanceiroService, SistemaFinanceiroService>();
+
+// SERVIÇO DOMINIO
+builder.Services.AddSingleton<InterfaceCategoria, RepositorioCategoria>();
+builder.Services.AddSingleton<InterfaceDespesa, RepositorioDespesa>();
+builder.Services.AddSingleton<InterfaceSistemaFinanceiro, RepositorioSistemaFinanceiro>();
+builder.Services.AddSingleton<InterfaceUsuarioSistemaFinanceiro, RepositorioUsuarioSistemaFinanceiro>();
+
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(option =>
