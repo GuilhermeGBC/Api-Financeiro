9497b0bf3325bc5522e77db37af23f99e6a5bb45
Guilherme Bastos <guicorrea188827@gmail.com>
2023-06-25 04:56:17 -0300

    add authorize

--- WebApi/Controllers/CategoriaController.cs
+++ WebApi/Controllers/CategoriaController.cs
@@ -1,11 +1,13 @@
 ﻿using Domain.Interfaces.ICategoria;
 using Domain.Interfaces.InterfaceServices;
 using Entities.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class CategoriaController : ControllerBase
