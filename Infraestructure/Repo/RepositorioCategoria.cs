37bc19fa9d5d2e372605bf4fd2308da3fae97676
Guilherme Bastos <guicorrea188827@gmail.com>
2023-06-25 02:26:58 -0300

    add repo categoria

--- Infraestructure/Repo/RepositorioCategoria.cs
+++ Infraestructure/Repo/RepositorioCategoria.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Domain.Interfaces.ICategoria;
+using Entities.Entities;
+using Infraestructure.Configuracao;
+using Infraestructure.Repo.Generics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design.Internal;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,26 @@ using System.Threading.Tasks;
 
 namespace Infraestructure.Repo
 {
-    internal class RepositorioCategoria
+    public class RepositorioCategoria : RepositoryGenerics<Categoria>, InterfaceCategoria
     {
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+
+        public RepositorioCategoria()
+        {
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
+        }
+
+        public async Task<IList<Categoria>> ListarCategoriasUsuario(string emailUsuario)
+        {
+            using (var db = new ContextBase(_OptionsBuilder))
+            {
+                return await 
+                    (from s in db.SistemaFinanceiro
+                     join c in db.Categoria on s.Id equals c.IdSistema
+                     join us in db.UsuarioSistemaFinanceiro on s.Id equals us.IdSistema
+                     where us.EmailUsuario.Equals(emailUsuario) && us.SistemaAtual
+                     select c).AsNoTracking().ToListAsync();
+            }
+        }
     }
 }
