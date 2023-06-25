78768cfa7e4cbeea63872e153e214ce743440992
Guilherme Bastos <guicorrea188827@gmail.com>
2023-06-25 05:05:18 -0300

    add despesa controller

---
+++ WebApi/Controllers/DespesasController.cs
@@ -0,0 +1,77 @@
+﻿using Domain.Interfaces.IDespesa;
+using Domain.Interfaces.InterfaceServices;
+using Entities.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DespesasController : ControllerBase
+    {
+        private readonly IDespesaService _despesaService;
+        private readonly InterfaceDespesa _interfaceDespesa;
+
+        public DespesasController(IDespesaService despesaService, InterfaceDespesa interfaceDespesa)
+        {
+            _despesaService = despesaService;
+            _interfaceDespesa = interfaceDespesa;
+        }
+
+        [HttpGet("/api/ListarDespesasUsuario")]
+        [Produces("application/json")]
+        public async Task<object> ListarDespesasUsuario(string emailUsuario)
+        {
+            return await _interfaceDespesa.ListarDespesasUsuario(emailUsuario);
+        }
+
+        [HttpPost("/api/AdicionarDespesa")]
+        [Produces("application/json")]
+        public async Task<object> AdicionarDespesa(Despesa despesa)
+        {
+            await _despesaService.AdicionarDespesa(despesa);
+            return despesa;
+        }
+
+        [HttpPost("/api/AtualizarDespesa")]
+        [Produces("application/json")]
+        public async Task<object> AtualizarDespesa(Despesa despesa)
+        {
+            await _despesaService.AtualizarDespesas(despesa);
+            return despesa;
+        }
+
+        [HttpGet("/api/ObterDespesa")]
+        [Produces("application/json")]
+        public async Task<object> ObterDespesa(int id)
+        {
+            return await _interfaceDespesa.GetEntityById(id);
+        }
+
+        [HttpDelete("/api/DeleteDespesa")]
+        [Produces("application/json")]
+        public async Task<object> DeleteDespesa(int id)
+        {
+            try
+            {
+                var despesa = await _interfaceDespesa.GetEntityById(id);
+                await _interfaceDespesa.Delete(despesa);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        [HttpDelete("/api/CarregaGraficos")]
+        [Produces("application/json")]
+        public async Task<object> CarregaGraficos(string emailUsuario)
+        {
+            return await _despesaService.CarregarGraficos(emailUsuario);
+        }
+    }
+}
